eb9530f57f63d3f875dd4646feaa35f10d79514f
Josh9602 <21harrisonjl@tonbridge-school.org>
2026-02-09 10:18:47 +0000

    2nd commit
    2nd commit

--- red.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-
-def exit_button(parent, button):
-    button0 = tk.Button(parent, bg = "red", command=parent.destroy)
-    button0.place(relx = 1.0, y = 0, anchor="ne", height = 10)
-    button0.update_idletasks()
